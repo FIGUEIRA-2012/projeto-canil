c6655494c713253cb119ccc23042b7859a73f9da
FIGUEIRA <figueira7481@gmail.com>
2022-07-30 13:39:01 -0300

    final projeto 1.0

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,14 +1,50 @@
 import express, {Request, Response} from 'express';
+import {createMenuObject} from '../helpers/createMenuObject';
+import {Pet} from '../models/pet';
 
 export const home = (req: Request, res: Response) =>{
-    res.send('home do controler');
+    let list = Pet.getAll();
+
+    res.render('pages/page',{
+        menu:createMenuObject('all'),
+        banner:{
+            title:'Todos os animais',
+            background:'allanimals.jpg'
+        },
+        list
+    });
+    
 };
 export const dogs = (req: Request, res: Response) =>{
-
+    let list = Pet.getFromtype('dog');
+    res.render('pages/page',{
+        menu:createMenuObject('dog'),
+        banner:{
+            title:'Cachorros',
+            background:'banner_dog.jpg'
+        },
+        list
+    });
 };
 export const cats = (req: Request, res: Response) =>{
-
+    let list = Pet.getFromtype('cat');
+    res.render('pages/page',{
+        menu:createMenuObject('cat'),
+        banner:{
+            title:'Gatos',
+            background:'banner_cat.jpg'
+        },
+        list
+    });
 };
 export const fishes = (req: Request, res: Response) =>{
-
+    let list = Pet.getFromtype('fish');
+    res.render('pages/page',{
+        menu:createMenuObject('fish'),
+        banner:{
+            title:'Peixes',
+            background:'banner_fish.jpg'
+        },
+        list
+    });
 };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,4 +9,8 @@ router.get('/', PageController.home);
 router.get('/dogs', PageController.dogs);
 router.get('/cats', PageController.cats);
 router.get('/fishes', PageController.fishes);
+
+router.get('/search', SearchController.search);
+
+
 export default router;
